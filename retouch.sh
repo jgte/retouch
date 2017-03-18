874b9358e5f645fc8e667862401304c0ff78db90
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2017-03-18 12:49:55 -0500

    fixed a bug with the directory being considered and all input arguments are passed to the find command

--- retouch.sh
+++ retouch.sh
@@ -7,10 +7,8 @@ function mtime()
 }
 
 #where am i?
-DIR=$(cd $(dirname $BASH_SOURCE);pwd)
-
 #loop over all files under current dir
-find $DIR -type f -print0 | while read -d $'\0' file
+find $PWD $@ -type f -print0 | while read -d $'\0' file
 do
   #stat -s <file> spits out a string ready to be eval'ed, something like:
   #st_dev=16777220 st_ino=80201143 st_mode=0100644 st_nlink=1 st_uid=503 st_gid=20 st_rdev=0 st_size=260 st_atime=1489856881 st_mtime=1489856881 st_ctime=1489856881 st_birthtime=1489856628 st_blksize=4096 st_blocks=8 st_flags=0
@@ -18,4 +16,5 @@ do
   touch $file || exit $?
   touch -a -t ${CURR_TIME[0]} $file || exit $?
   touch -m -t ${CURR_TIME[1]} $file || exit $?
+  ls -la $file
 done
